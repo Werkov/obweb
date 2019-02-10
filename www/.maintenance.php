4ab1b97426cffb6dfcd615b8ee5b8f6911d554fd
Michal Koutný <xm.koutny@gmail.com>
2019-02-10 22:46:34 +0100

    Add maintenance page

---
+++ www/.maintenance.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Údržba</title>
+	<meta name="charset" value"utf-8" />
+</head>
+<body>
+<h1>Údržba</h1>
+<p>Probíhá migrace PHP 5.3 na PHP 5.6.</p>
+
+<p>2019-02-10</p>
+
+
+</body>
+</html>
+<?php
+exit();
